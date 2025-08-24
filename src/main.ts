5a79406ec0f1de8a44cfabb9ceadab2f5c67036b
deboak <adebowaleakinode9@gmail.com>
2025-08-24 15:59:17 +0100

    fix vercel build config

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
-  const port = process.env.PORT || 3100;
-  await app.listen(port, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3100);
 }
 bootstrap();
